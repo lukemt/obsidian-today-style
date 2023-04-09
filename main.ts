4d916f46286ebfeaefb6005ff934a9b86b7bf3ef
Lukas Petry <git.luke@rennwurst.de>
2023-04-09 11:18:34 +0200

    implementation

--- main.ts
+++ main.ts
@@ -12,58 +12,11 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	stylesheet: HTMLStyleElement | null = null;
+	today: string | null = null; // example: 2021-01-01
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
@@ -75,11 +28,44 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(window.setInterval(() => this.updateStylesheetIfDateChanged(), 5 * 60 * 1000));
+
+		this.updateStylesheetIfDateChanged();
 	}
 
 	onunload() {
+		this.detachStylesheet();
+	}
 
+	detachStylesheet() {
+		if (this.stylesheet) {
+			this.stylesheet.remove();
+			this.stylesheet = null;
+		}
+	}
+	updateStylesheet(date: string) {
+		this.detachStylesheet();
+		this.stylesheet = document.createElement('style');
+		this.stylesheet.innerHTML = `
+			.mod-root .mod-stacked .workspace-tab-header:first-child:not([aria-label="${date}"]) {
+				background-color: rgba(255, 0, 38, 0.4) !important;
+			}
+			.mod-root .mod-stacked .workspace-tab-header:first-child[aria-label="${date}"] {
+				background-color: #00ffb30a !important;
+			}
+			.mod-root .workspace-tab-header[aria-label="${date}"] + .workspace-leaf .HyperMD-header-1 {
+    		color: var(--h3-color);
+			}
+		`;
+		document.head.appendChild(this.stylesheet);
+	}
+
+	updateStylesheetIfDateChanged() {
+		const today = new Date().toISOString().slice(0, 10);
+		if (today !== this.today) {
+			this.today = today;
+			this.updateStylesheet(today);
+		}
 	}
 
 	async loadSettings() {
@@ -88,22 +74,6 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
